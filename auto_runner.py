645c98678f5de70bec79822f6357b5fc55791434
Galileo Sartor <galileo.sartor@gmail.com>
2025-09-04 22:31:59 +0100

    Set running short term plan with the goal destination.
    This should then enable short diversions (in the same window of the short path).
    Move the world state and actions to common.asp

--- auto_runner.py
+++ auto_runner.py
@@ -18,14 +18,14 @@ class ClingoApp(Application):
         print("Starting optimization...\n")
         self.select_action()
 
-    def get_optimum_trace(self, global_asp_path, start, ignore=None):
+    def get_optimum_trace(self, global_asp_path, start, start_time, ignore=None):
         ctl = clingo.Control([f"--parallel-mode={cpu_count}"])
         ctl.load(global_asp_path)
-        ctl.add("base", [], f"fact(state(0, position({start}))).")
+        ctl.add("base", [], f"fact(state({start_time}, position({start}))).")
         # This should live elsewhere
         if ignore:
             facts = ignore[:2]
-            print(f"Ignoring facts: {facts}", "\n")
+            print(f"Ignoring facts: {[str(x) for x in facts]}", "\n")
             A = facts[0].arguments[1].arguments[0].name
             B = facts[1].arguments[1].arguments[0].name
             # Exclude the ignored short path. From the ASP code
@@ -58,19 +58,24 @@ class ClingoApp(Application):
             arguments = atom.arguments
             facts.append(Function("global_trace", arguments))
         # print("\n", "Input trace atoms:", type(atoms))
-        print("Extracted trace atoms:", facts, "\n")
+        # print("Extracted trace atoms:", facts, "\n")
         facts.sort(key=lambda x: int(x.arguments[0].number))
         # There has to be a better way to do this...
-        string_facts = [str(x) + "." for x in facts]
-        print(f"Converted to global_trace facts: {string_facts}", "\n")
-        return string_facts[:2]
+        # string_facts = [str(x) + "." for x in facts]
+        # print(f"Converted to global_trace facts: {string_facts}", "\n")
+        return facts[:2]
 
-    def get_local_trace(self, local, start, facts):
-        print("Getting local trace with facts:", facts, " from ", start, "\n")
+    def get_local_trace(self, local, start, start_time, facts):
+        string_facts = [str(x) + "." for x in facts]
+        goal = facts[-1].arguments[-1].arguments[0]
+        print("Getting local trace with facts:", string_facts, "from", start, "to", goal, "at", start_time, "\n")
         ctl = clingo.Control(["--warn=none", f"--parallel-mode={cpu_count}"])
         ctl.load(local)
-        ctl.add("base", [], f"fact(state(0, position({start}))).")
-        ctl.add("base", [], '\n'.join(facts))
+        ctl.add("base", [], f"fact(state({start_time}, position({start}))).")
+        ctl.add("base", [], f"fact(state({start_time}, direction(forward))).")
+        ctl.add("base", [], f"time({start_time}..{start_time+2}).")
+        ctl.add("base", [], f"goal :- fact(state(_, position({goal}))).\n:- not goal.")
+        ctl.add("base", [], '\n'.join(string_facts))
         ctl.ground([("base", [])])
         proposed_models = []
 
@@ -82,41 +87,48 @@ class ClingoApp(Application):
         ctl.solve(on_model=on_model)
         return proposed_models
 
-    def run_short_path(self, global_trace, start):
+    def destination_reached(self):
+        print("Destination reached! \N{smiling face with sunglasses}", "\n")
+
+    def run_short_path(self, global_trace, start, start_time):
         facts = self.trace_atoms_to_global_trace(global_trace)
-        proposed_model = self.get_local_trace(local_asp, start, facts)
+        proposed_model = self.get_local_trace(local_asp, start, start_time, facts)
         if not proposed_model:
             print("No proposed model found.", "\n")
-            return
-        if len(proposed_model[0]) == 0:
-            print("Destination reached.", "\n")
-            return
-        print(f"Proposed {len(proposed_model)} local trace model: {proposed_model[0]}", "\n")
-        # For now we assume we do not want violations
-        # To be refined later as part of the ASP code
-        # Replace with short term planner with preference. If optimize by violations (with specific number)
-        # then fail if returns no possible models.
-        # The short term planner should also recommend not breaking the rule, so waiting at the light.
-        # We can assume for now that the increase in time to avoid a violation is 1 and recalculate with that info.
-        if "violation" in str(proposed_model[0]):
-            print("Violation detected in the proposed model.", "\n")
-            # Hopefully this does not loop forever
-            self.select_action(ignore=global_trace)
+            # This shouldn't happen, but if there is no valid short term plan at all then call the long term planner again.
+            self.select_action(ignore=global_trace, start=start, start_time=start_time)
+        # If the proposed model is < 2 it means that either I am at my destination, or a single move will take me there.
+        # Change with the short term plan.
+        elif len(proposed_model[0]) < 2:
+            self.destination_reached()
         else:
-            print("No violation detected. Action accepted. Continuing...", "\n")
-            new_trace = global_trace[2:]
-            new_start = global_trace[1].arguments[-1].arguments[0].name
-            print("Remaining trace", new_trace, " from ", new_start, "\n")
-            self.run_short_path(new_trace, new_start)
+            print(f"Proposed {len(proposed_model)} local trace model: {proposed_model[0]}", "\n")
+            # For now we assume we do not want violations
+            # Replace with short term planner with preference as input. Should work by tweaking the minimize instructions
+            # This then launches a global trace if there is no valid short term path
+            if "violation" in str(proposed_model[0]):
+                print("Violation detected in the proposed model. Rerouting from", start, start_time, "\n")
+                # Hopefully this does not loop forever
+                self.select_action(ignore=global_trace, start=start, start_time=start_time)
+            else:
+                print("No violation detected. Action accepted. Continuing...", "\n")
+                new_trace = global_trace[2:]
+                if new_trace == []:
+                    self.destination_reached()
+                    return
+                new_start = global_trace[1].arguments[-1].arguments[0].name
+                new_start_time = start_time + 2
+                print("Remaining trace", [str(x) for x in new_trace], " from ", new_start, "\n")
+                self.run_short_path(new_trace, new_start, new_start_time)
 
-    def select_action(self, ignore=None, start="a"):
-        models, cost = self.get_optimum_trace(global_asp, start, ignore)
+    def select_action(self, ignore=None, start="a", start_time=0):
+        models, cost = self.get_optimum_trace(global_asp, start, start_time, ignore)
         print(f"Found {len(models)} optimum models with cost {cost}.", "\n")
         if models:
             global_trace = list(models[0])
             global_trace.sort(key=lambda x: x.arguments[0].number)
-            print("Global trace:", global_trace, "\n")
-            self.run_short_path(global_trace, start)
+            print("Global trace:", [str(x) for x in global_trace], "\n")
+            self.run_short_path(global_trace, start, start_time)
         else:
             print("No optimum trace found.", "\n")
 
--- local.asp
+++ local.asp
@@ -1,6 +1,8 @@
 #include "Deontic/defeasible-ap.asp".
 #include "Deontic/deontic-comp.asp".
 
+#include "common.asp".
+
 % -------------------------------------
 % Rule r1/r2: Obligation to stop at traffic lights if red and not emergency
 % -------------------------------------
@@ -21,81 +23,37 @@ atom(stop(T, X)) :- tf(T, X).
 tf(T, X) :- fact(traffic_light(ID, X)), fact(state(T, traffic_light(ID, red))).
 
 % -------------------------------------
-% World Modelling
-% -------------------------------------
-
-time(0..2).
-
-connected(a,b).
-connected(b,g).
-connected(a,c).       
-connected(c,d).
-connected(d,e).
-connected(e,g).
-connected(a,x).
-connected(x,y).
-connected(y,z).
-
-fact(traffic_light(tl1, b)).
-fact(traffic_light(tl2, d)).
-
-fact(gas_station(gs1, y)).
-fact(gas_station(gs2, e)).
-
-next(T, X, Y) :- fact(state(T, direction(forward))), connected(X, Y).
-next(T, X, Y) :- fact(state(T, direction(backward))), connected(Y, X).
-
-invert(backward, forward).
-invert(forward, backward).
-
-% -------------------------------------
 % World State
 % -------------------------------------
 
-% fact(state(0, position(a))).
-fact(state(0, fuel(2))).
+%time(0..2).
+
+%fact(state(0, fuel(2))).
 fact(state(0, direction(forward))).
 fact(state(0, traffic_light(tl1, red))).
 
-fact(state(T, traffic_light(tl2, green))) :- time(T), T \ 14 = 0.
--fact(state(T, traffic_light(tl2, red))) :- time(T), T \ 14 = 0.
+% fact(state(T, traffic_light(tl2, red))) :- time(T), T \ 14 = 3.
+% -fact(state(T, traffic_light(tl2, green))) :- time(T), T \ 14 = 3.
+% fact(state(T, traffic_light(tl2, green))) :- time(T), T \ 14 = 5.
+% -fact(state(T, traffic_light(tl2, red))) :- time(T), T \ 14 = 5.
 fact(state(T, traffic_light(tl2, red))) :- time(T), T \ 14 = 7.
 -fact(state(T, traffic_light(tl2, green))) :- time(T), T \ 14 = 7.
-
-
-% -------------------------------------
-% Available Actions
-% -------------------------------------
-
-% Preconditions
-action(T, move(Y)) :- fact(state(T, position(X))), fact(state(T, fuel(F))), F > 0, next(T, X, Y), time(T+1).
-action(T, invert(X)) :- fact(state(T, position(X))), time(T+1).
-action(T, refuel(X)) :- fact(state(T, position(X))), fact(gas_station(_, X)), time(T+1).
-
-% Postconditions
-fact(state(T+1, position(X))) :- trace(T, move(X)).
-fact(state(T+1, fuel(Y-1))) :- trace(T, move(_)), fact(state(T, fuel(Y))).
-fact(state(T+1, direction(ND))) :- trace(T, invert(X)), fact(state(T, direction(D))), invert(D, ND).
-fact(state(T+1, fuel(5))) :- trace(T, refuel(X)).
-
--fact(state(T+1, position(D))) :- trace(T, move(_)), fact(state(T, position(D))).
--fact(state(T+1, direction(D))) :- trace(T, invert(_)), fact(state(T, direction(D))).
--fact(state(T+1, fuel(D))) :- trace(T, refuel(_)), fact(state(T, fuel(D))).
--fact(state(T+1, fuel(D))) :- trace(T, move(_)), fact(state(T, fuel(D))).
-
-% Inertia
-fact(state(T+1, X)) :- fact(state(T, X)), time(T+1), not -fact(state(T+1, X)).
 
 % -------------------------------------
 % Planning Problem
 % -------------------------------------
 
-% global_trace(0,move(b)). 
-% global_trace(1,move(g)).
+%time(0..4).
+%fact(state(0, position(a))).
+% Must reach position g
 
+%goal :- fact(state(_, position(g))).
+%:- not goal.
+
+% 0 { trace(T, X) : action(T, X) } 1 :- time(T).
 1 { trace(T, X) : time(T), action(T, X) } 1 :- global_trace(GT, X).
 
-:- global_trace(GT1, X1), global_trace(GT2, X2), GT1 < GT2, trace(T1, X1), trace(T2, X2), T1 >= T2.
+% :- global_trace(GT1, X1), global_trace(GT2, X2), GT1 < GT2, trace(T1, X1), trace(T2, X2), T1 >= T2.
 
 % fact(emergency).
 
